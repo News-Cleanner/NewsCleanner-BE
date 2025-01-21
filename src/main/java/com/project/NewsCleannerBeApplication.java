d38d69a90c92c800b1dc4ee7267e3b645ec0e129
awarduuu <awarduuu@gmail.com>
2025-01-21 22:13:56 +0900

    Initial Setting

---
+++ src/main/java/com/project/NewsCleannerBeApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCleannerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsCleannerBeApplication.class, args);
+	}
+
+}
